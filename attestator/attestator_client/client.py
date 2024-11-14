eabc244f59b664ac8beb0b454d97990497e6ae5b
Ulisse Bordignon <ulisse.bordignon@protonmail.com>
2024-11-14 10:35:45 +0100

    ref(client): forward commands to the server directly

--- attestator/attestator_client/client.py
+++ attestator/attestator_client/client.py
@@ -4,9 +4,6 @@ import logging
 
 
 from ..messages import (
-    ERROR_RESPONSE,
-    INVALID_REQUEST_TYPE,
-    PING,
     VSockRequest,
     VSockResponse,
     dict_to_vsock_message,
@@ -25,15 +22,12 @@ class AttestatorClient:
         address, port = writer.get_extra_info("socket").getsockname()
         logger.info("Client started on %s:%d", address, port)
 
-        if cmd == PING:
-            ping = VSockRequest(request_type=PING, args=[]).as_dict()
-            logger.info("Sending ping message")
-            writer.writelines([dict_to_vsock_message(ping)])
-            await writer.drain()
+        request = VSockRequest(request_type=cmd, args=cmd_args).as_dict()
+        logger.info("Sending request")
+        writer.writelines([dict_to_vsock_message(request)])
+        await writer.drain()
 
-            return VSockResponse.from_json(
-                vsock_message_to_dict(await reader.readline())
-            )
-        return VSockResponse(
-            response_type=ERROR_RESPONSE, response=[INVALID_REQUEST_TYPE]
-        )
+        resp = await reader.readline()
+        logger.info("Received response %s", resp)
+
+        return VSockResponse.from_json(vsock_message_to_dict(resp))
