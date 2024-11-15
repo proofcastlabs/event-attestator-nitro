79501cea6ee6d1aa4f01cd57dd9f29d31934cad0
Ulisse Bordignon <ulisse.bordignon@protonmail.com>
2024-11-15 17:04:17 +0100

    feat(chain): add `find_consensus_element` util

--- attestator/chain/utils.py
+++ attestator/chain/utils.py
@@ -18,3 +18,24 @@ def create_chain_state_from_config(chain, config):
             chain, config["events"], config["consensus_threshold"], config["endpoints"]
         )
     raise ChainException(f"Protocol {protocol} not supported")
+
+
+def find_consensus_element(data, threshold):
+    """Return an element from `data` if at least `threshold` elements match it.
+
+    `threshold` needs to be at least `len(data) // 2 + 1` to make sense."""
+    # If no consensus element is found in the first half + 1 of the data, none will be
+    # I'm checking half **plus one** to cover for odd `data` lengths
+    cutoff = len(data) // 2 + 1
+    consensus = None
+    for idx, e in enumerate(data[:cutoff]):
+        counter = 1
+        # Checking forward only, no point in checking prior elements, since I already did
+        for ee in data[idx + 1 :]:
+            # This works by overloading `==` with `__eq__`
+            if e == ee:
+                counter += 1
+        if counter >= threshold:
+            consensus = e
+            break
+    return consensus
