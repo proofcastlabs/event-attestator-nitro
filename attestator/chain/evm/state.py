3465f719acaeaf7de1bd5511e17be1e3d6628c24
Ulisse Bordignon <ulisse.bordignon@protonmail.com>
2024-11-21 16:03:02 +0100

    feat(evm): add `filter_transaction` to `EvmState`

--- attestator/chain/evm/state.py
+++ attestator/chain/evm/state.py
@@ -11,3 +11,13 @@ class EvmState(ChainState):
         self.events = events
         self.threshold = event_consensus_threshold
         self.rpcs = rpcs
+
+    def filter_transaction(self, transaction):
+        """Return alls the logs that match the events schema stored."""
+        matching_logs = []
+        for log in transaction.logs:
+            for address, topic in self.events:
+                if log.address == address.lower() and topic.lower() in log.topics:
+                    matching_logs.append(log)
+                    break
+        return matching_logs
